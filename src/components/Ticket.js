edde0ce0f622b8afce25d0263430120f600b274f
Matt Casperson <mc.casperson@gmail.com>
2022-02-07 09:17:14 -0800

    initialize file structure, add props to tickets
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

---
+++ src/components/Ticket.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Ticket(props){
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
