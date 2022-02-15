f5c7798d02ae67651f31e61dbdae104fca993b80
Matt Casperson <mc.casperson@gmail.com>
2022-02-15 08:22:59 -0800

    add styling to reusable form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -4,18 +4,21 @@ import PropTypes from "prop-types";
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
+      <form onSubmit={props.formSubmissionHandler} className="form-control">
         <input
           type='text'
           name='names'
-          placeholder='Pair Names' />
+          placeholder='Pair Names' 
+          className="form-control" />
         <input
           type='text'
           name='location'
-          placeholder='Location' />
+          placeholder='Location' 
+          className="form-control" />
         <textarea
           name='issue'
-          placeholder='Describe your issue.' />
+          placeholder='Describe your issue.' 
+          className="form-control" />
         <button type='submit' className="btn btn-warning">{props.buttonText}</button>
       </form>
     </React.Fragment>
