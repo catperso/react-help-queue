8c4e30262ffcdda8a758051fe8b6188501c6ee95
Matt Casperson <mc.casperson@gmail.com>
2022-02-21 09:46:27 -0800

    refactor TicketList.js to use firestore database

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,27 +1,64 @@
 import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from 'prop-types';
+// We need to import hooks functionality from both react-redux and react-redux-firebase.
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+
+// function TicketList(props){
+//   return (
+//     <React.Fragment>
+//       <hr/>
+//       {Object.values(props.ticketList).map((ticket) =>
+//         <Ticket
+//           whenTicketClicked = { props.onTicketSelection }
+//           names={ticket.names}
+//           location={ticket.location}
+//           issue={ticket.issue}
+//           formattedWaitTime={ticket.formattedWaitTime}
+//           id={ticket.id}
+//           key={ticket.id}/>
+//       )}
+//     </React.Fragment>
+//   );
+// }
 
 function TicketList(props){
-  return (
-    <React.Fragment>
-      <hr/>
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket
-          whenTicketClicked = { props.onTicketSelection }
-          names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          formattedWaitTime={ticket.formattedWaitTime}
-          id={ticket.id}
-          key={ticket.id}/>
-      )}
-    </React.Fragment>
-  );
+  // The useFirestoreConnect() hook comes from react-redux-firebase.
+  useFirestoreConnect([ { collection: 'tickets' } ]);
+
+  // The useSelector() hook comes from react-redux.
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+
+  // react-redux-firebase also offers a useful isLoaded() function.
+  if (isLoaded(tickets)) {
+    return (
+      <React.Fragment>
+        <hr/>
+        {tickets.map((ticket) => {
+          return <Ticket
+            whenTicketClicked = { props.onTicketSelection }
+            names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            formattedWaitTime={ticket.formattedWaitTime}
+            id={ticket.id}
+            key={ticket.id}/>
+        })}
+      </React.Fragment>
+    );
+  // If the tickets aren't loaded yet, our fragment will return a "Loading..." message.
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
+  // ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
